2a912fc94279a4af4367882c8c491ed0a1288b5f
马洪彪 <e_yuan@foxmail.com>
2021-04-21 09:03:59 +0800

    1

---
+++ LimsCrystalReport/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LimsCrystalReport
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.btnExportAll = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // bgw
+            // 
+            this.bgw.WorkerReportsProgress = true;
+            this.bgw.WorkerSupportsCancellation = true;
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw_ProgressChanged);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // btnExportAll
+            // 
+            this.btnExportAll.Location = new System.Drawing.Point(13, 13);
+            this.btnExportAll.Name = "btnExportAll";
+            this.btnExportAll.Size = new System.Drawing.Size(77, 31);
+            this.btnExportAll.TabIndex = 0;
+            this.btnExportAll.Text = "导出报表";
+            this.btnExportAll.UseVisualStyleBackColor = true;
+            this.btnExportAll.Click += new System.EventHandler(this.btnExportAll_Click);
+            // 
+            // pb
+            // 
+            this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb.Location = new System.Drawing.Point(13, 62);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(776, 23);
+            this.pb.TabIndex = 1;
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 113);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.btnExportAll);
+            this.Name = "fmMain";
+            this.Text = "Starlims报表工具";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Button btnExportAll;
+        private System.Windows.Forms.ProgressBar pb;
+    }
+}
+
---
+++ LimsCrystalReport/Form1.cs
@@ -0,0 +1,109 @@
+﻿using LimsCrystalReport.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace LimsCrystalReport
+{
+    public partial class fmMain : Form
+    {
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ExportAll();
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pb.Value++;
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnExportAll.Enabled = true;
+        }
+
+        private void btnExportAll_Click(object sender, EventArgs e)
+        {
+            btnExportAll.Enabled = false;
+            //
+            string sql = "select count(1) from limsreports " +
+                "where categoryid not in " +
+                "(select categoryid from limsreportcategories " +
+                "where cattype = 'AA')";
+            int count = 1;
+            //count = (int)SQLHelper.ExecuteScalar(sql);
+            count = int.Parse(OracleHelper.ExecToSqlGetTable(sql).Rows[0][0].ToString());
+            pb.Maximum = count;
+            //
+            bgw.RunWorkerAsync();
+        }
+
+        private void ExportAll()
+        {
+            string exportBasePath = @"D:\Workspace\LimsCrystalReport\Export\";
+            exportBasePath = ConfigurationManager.AppSettings["exportBasePath"];
+            //
+            string sql = @"select rc.displaytext, rc.categoryid
+                          from limsreportcategories rc where rc.cattype = 'RPT'";
+            DataTable dtReportCategory = OracleHelper.ExecToSqlGetTable(sql);
+            foreach(DataRow drReportCategory in dtReportCategory.Rows)
+            {
+                string categoryid = drReportCategory["CATEGORYID"].ToString();
+                //
+                string exportPath = exportBasePath + drReportCategory["DISPLAYTEXT"].ToString() + @"/";
+                Directory.CreateDirectory(exportPath);
+                string exportCHSPath = exportPath + @"CHS/";
+                Directory.CreateDirectory(exportCHSPath);
+                string exportENGPath = exportPath + @"ENG/";
+                Directory.CreateDirectory(exportENGPath);
+                //
+                sql = @"select r.reportid, r.displaytext
+                    from limsreports r 
+                    where r.categoryid = {0}";
+                DataTable dtReport = OracleHelper.ExecToSqlGetTable(string.Format(sql, ":CATEGORYID")
+                    , new OracleParameter[] { new OracleParameter(":CATEGORYID", categoryid) });
+                int iReport = 0;
+                foreach(DataRow drReport in dtReport.Rows)
+                {
+                    string reportid = drReport["REPORTID"].ToString();
+                    //
+                    string exportCHSFile = exportCHSPath+ drReport["DISPLAYTEXT"].ToString() + ".rpt";
+                    string exportENGFile = exportENGPath + drReport["DISPLAYTEXT"].ToString() + ".rpt";
+                    //
+                    sql = @"select rd.report,rd.langid
+                        from limsreportdocuments rd 
+                        where 1=1 and rd.reportid = {0}";
+                    DataTable dtReportDoc = OracleHelper.ExecToSqlGetTable(string.Format(sql, ":REPORTID")
+                    , new OracleParameter[] { new OracleParameter(":REPORTID", reportid) });
+                    foreach(DataRow drReportDoc in dtReportDoc.Rows)
+                    {
+                        string langid = drReportDoc["LANGID"].ToString();
+                        byte[] reportDocContent = (byte[])drReportDoc["REPORT"];
+                        if (langid == "CHS")
+                        {
+                            File.WriteAllBytes(exportCHSFile, reportDocContent);
+                        }else if(langid == "ENG")
+                        {
+                            File.WriteAllBytes(exportENGFile, reportDocContent);
+                        }
+                    }
+                    bgw.ReportProgress(++iReport);
+                }
+            }
+        }
+    }
+}
